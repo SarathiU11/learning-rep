32bdef29fc02b597775cab268ad8f2cf20e5ef7d
Sarathi U <sarathiu11@gmail.com>
2025-07-28 20:48:35 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+##
+
+#Write a Python program that asks the user to input the following information:
+
+#Their full name
+
+#Their age
+
+#Their favorite programming language
+
+#Then, print a sentence in the following format:
+
+
+
+#Hello, <name>! You are <age> years old and you love <language>.
+
+
+name =str(input("enter the name:"))
+age=int(input("enter the  name:"))
+language=str(input("enter the programming language:"))
+
+print(f"Hello, {name}! You are {age} years old and you love {language}.")
